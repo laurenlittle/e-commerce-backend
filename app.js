52a510ea495042e5cd84c399246d011be87dae6a
laurenlittle <littlelauren016@gmail.com>
2019-08-17 09:39:53 -0400

    import db and move routes

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// Import Routes
+const userRoutes = require('./routes/user');
+
+// app
 const app = express();
 
-require('dotenv').config();
+// db
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+}).then(() => {
+  console.log('DB Connected');
+})
+
+// Routes Middleware
+app.use('/api', userRoutes);
 
 // Node JS has process
 const port = process.env.PORT || 8000
-
-app.get('/', (req, res) => {
-  res.send('hello from node');
-});
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
