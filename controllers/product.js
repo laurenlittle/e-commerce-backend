9b78c90ee3b01faeadd11d31c14641aa6f1ca026
laurenlittle <littlelauren016@gmail.com>
2019-08-31 15:22:35 -0400

    add controller method to list products by search

--- controllers/product.js
+++ controllers/product.js
@@ -264,3 +264,60 @@ exports.listCategories = (req, res) => { // Get all the categories that are used
     res.json(categories);
   });
 };
+
+
+/**
+ * list products by search (implemented in frontend)
+ * show categories in checkbox and price range in radio buttons user can click
+ * make api request and show the products to users based on selection
+ */
+
+exports.listBySearch = (req, res) => {
+
+  // Query Params
+  let order = req.body.order ? req.body.order : "desc";
+  let sortBy = req.body.sortBy ? req.body.sortBy : "_id";
+  let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+  let skip = parseInt(req.body.skip);
+  let findArgs = {}; // This object will contain category IDs and price range.
+
+  // console.log(order, sortBy, limit, skip, req.body.filters);
+  // console.log("findArgs", findArgs);
+
+  for (let key in req.body.filters) {
+    if (req.body.filters[key].length > 0) {
+      if (key === "price") {
+        // gte -  greater than price [0-10] (gte/lte avail via Mongo)
+        // lte - less than
+        findArgs[key] = {
+          $gte: req.body.filters[key][0],
+          $lte: req.body.filters[key][1]
+        };
+      } else {
+        findArgs[key] = req.body.filters[key];
+      }
+    }
+  }
+
+  Product.find(findArgs)
+    .select("-photo")
+    .populate("category")
+    .sort([
+      [sortBy, order]
+    ])
+    .skip(skip)
+    .limit(limit)
+    .exec((err, data) => {
+      if (err) {
+        return res.status(400).json({
+          error: "Products not found"
+        });
+      }
+
+      res.json({
+        size: data.length, // how many products their are
+        data
+      });
+    });
+
+};
--- routes/product.js
+++ routes/product.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, read, remove, update, list, productById, listRelated, listCategories } = require('../controllers/product');
+const { create, read, remove, update, list, productById, listRelated, listCategories, listBySearch } = require('../controllers/product');
 const {requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
 router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create);
-router.get('/product/:productId', read);
+router.post('/products/by/search', listBySearch); // sending filters in request body - post method needed
 router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, remove);
 router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update);
+router.get('/product/:productId', read);
 router.get('/products', list);
 router.get('/products/related/:productId', listRelated);
 router.get('/products/categories', listCategories);
