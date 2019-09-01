4847aa52410a28db49cf73646878d3c824333719
laurenlittle <littlelauren016@gmail.com>
2019-08-31 23:03:50 -0400

    allow user to view/update profile

--- controllers/user.js
+++ controllers/user.js
@@ -14,3 +14,33 @@ exports.userById = (req, res, next, id) => {
     next();
   })
 }
+
+
+exports.read = (req, res) => {
+  req.profile.hashed_password = undefined;
+  req.profile.salt = undefined;
+
+  return res.json(req.profile);
+};
+
+
+exports.update = (req, res)  => {
+  // find user by Id - use set method with updated fields/info in request body
+  User.findOneAndUpdate(
+    { _id: req.profile._id },
+    { $set: req.body },
+    { new: true },
+    (err, user) => {
+      if (err) {
+        return res.status(400).json({
+          error: 'Not authorized to perform this action.'
+        });
+      }
+
+      user.hashed_password = undefined;
+      user.salt = undefined;
+      res.json(user);
+    }
+  );
+
+};
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { userById } = require('../controllers/user');
+const { userById, read, update } = require('../controllers/user');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 
 // @TODO - Fix This - test Route isn't working in Insomnia - 403 errors always
@@ -10,6 +10,11 @@ router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
     user: req.profile
   });
 });
+
+router.get('/user/:userId', requireSignin, isAuth, read); // view profile
+router.put('/user/:userId', requireSignin, isAuth, update); // update profile
+
+
 
 router.param('userId', userById); // when there's a userId in route params, findById will run and make info available in request object
 
