a0704c4144a681f513fef1d87ce70e6bff512be6
laurenlittle <littlelauren016@gmail.com>
2019-08-31 23:07:33 -0400

    add CORS

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const morgan = require('morgan');
 const expressValidator = require('express-validator');
 require('dotenv').config();
@@ -28,6 +29,7 @@ app.use(morgan('dev')); // HTTP request logger middleware for node.js
 app.use(bodyParser.json()); // get the json data from request body
 app.use(cookieParser()); // save user creds in cookie
 app.use(expressValidator()); // validate user data on signup
+app.use(cors()); // API can handle requests coming from different origin (i.e request from 3001 react to 8000 backend)
 
 // Routes Middleware
 app.use('/api', authRoutes);
