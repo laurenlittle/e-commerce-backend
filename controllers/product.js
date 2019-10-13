1fd5b9634a581a0c6ebf988781fe396852b55c95
laurenlittle <littlelauren016@gmail.com>
2019-10-13 09:24:50 -0400

    populate category for specific productId

--- controllers/product.js
+++ controllers/product.js
@@ -65,7 +65,9 @@ exports.create = (req, res) => {
 
 exports.productById = (req, res, next, id) => {
 
-  Product.findById(id).exec((err, product) => {
+  Product.findById(id)
+  .populate('category')
+  .exec((err, product) => {
     if (err || !product) {
       return res.status(400).json({
         error: 'Product not found'
