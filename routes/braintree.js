c26863b856183688a3d374ff8cb05e2c36a47e33
laurenlittle <littlelauren016@gmail.com>
2019-10-22 05:53:50 -0400

    update app.js to contain braintree routes; create braintree routes/controller method to get clientToken for FE

---
+++ routes/braintree.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+require('dotenv').config();
+
+const { requireSignin, isAuth } = require('../controllers/auth');
+const { userById } = require ('../controllers/user');
+const { generateToken } = require('../controllers/braintree');
+
+
+// available for auth users only
+router.get('/braintree/getToken/:userId', requireSignin, isAuth, generateToken);
+
+router.param('userId', userById); // when there's a userId in route params, findById will run and make info available in request object
+
+module.exports = router;
