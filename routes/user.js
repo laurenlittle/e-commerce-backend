35acb446e3d3088bc02c04775951038a3d7ec5fe
laurenlittle <littlelauren016@gmail.com>
2019-11-03 08:31:35 -0500

    create GET route for purchaseHistory and add as middleware

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-const { userById, read, update } = require('../controllers/user');
-const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
+const {
+  userById,
+  read,
+  update,
+  purchaseHistory
+} = require('../controllers/user');
 
-// @TODO - Fix This - test Route isn't working in Insomnia - 403 errors always
+const {
+  requireSignin,
+  isAuth,
+  isAdmin
+} = require('../controllers/auth');
+
 router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
   res.json({
     user: req.profile
@@ -13,8 +22,7 @@ router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
 
 router.get('/user/:userId', requireSignin, isAuth, read); // view profile
 router.put('/user/:userId', requireSignin, isAuth, update); // update profile
-
-
+router.get('/orders/by/user/:userId', requireSignin, isAuth, purchaseHistory);
 
 router.param('userId', userById); // when there's a userId in route params, findById will run and make info available in request object
 
